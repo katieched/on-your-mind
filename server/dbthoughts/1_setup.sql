c2b96f7e2906824aa934763f66d1b152438eaa77
Katie Chedgzoy <katieched98@hotmail.co.uk>
2021-10-20 19:54:02 +0100

    empty branch

--- server/dbthoughts/1_setup.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS thoughts;
-
-CREATE TABLE thoughts (
-    id serial PRIMARY KEY,
-    title varchar(255),
-    pseudonym varchar(255),
-    post_body varchar(500)
-);
-
--- server/dbthoughts/2_seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO thoughts (title, pseudonym, post_body)
-VALUES
-('My thoughts', 'Katie', 'I had a good day'), 
-('My day', 'Hamsa', 'I had a bad day');
