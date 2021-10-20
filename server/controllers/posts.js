1b4f0ae76f5bb6699f1e58f495483356bf8a03af
Katie Chedgzoy <katieched98@hotmail.co.uk>
2021-10-20 10:22:23 +0100

    add initial files

---
+++ server/controllers/posts.js
@@ -0,0 +1,7 @@
+// Make routes
+
+//get route to show all posts
+
+//get route to show specific post
+
+//post route to create post
---
+++ server/models/posts.js
@@ -0,0 +1,7 @@
+//making the class
+
+//get all function
+
+//find by id function to show specific post
+
+//function that creates post
