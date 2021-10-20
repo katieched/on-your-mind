1035e4ac9a6e3389fd11df4910f11fea6877292f
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-20 19:40:40 +0100

    add client yaml

--- server/dbthoughts/2_seed.sql
+++ server/dbthoughts/2_seed.sql
@@ -1,4 +1,4 @@
 INSERT INTO thoughts (title, pseudonym, post_body)
 VALUES
-('Mythoughts', 'Katie', 'I had a good day'), 
+('My thoughts', 'Katie', 'I had a good day'), 
 ('My day', 'Hamsa', 'I had a bad day');
