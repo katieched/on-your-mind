053787e4bb06b217f4cd910d8b9b828ad17f2996
hamsamuxumed <83721832+hamsamuxumed@users.noreply.github.com>
2021-10-20 15:02:31 +0100

    connected db to server

--- server/db/1_setup.sql
+++
@@ -1,9 +0,0 @@
-DROP TABLE IF EXISTS postsdb;
-
-CREATE TABLE postsdb (
-    id serial PRIMARY KEY,
-    title varchar(255) NOT NULL,
-    name varchar(255) NOT NULL,
-    post_body varchar(255) NOT NULL
-);
-
--- server/db/2_seed.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO postsdb (title, name, post_body)
-VALUES
-    ('Katie', 'My thoughts', 'I had a good day'),
-    ('Hamsa', 'My day', 'I had a bad day');
